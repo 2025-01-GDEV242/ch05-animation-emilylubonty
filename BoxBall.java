8c6b7100b319598a140ca19affb7b7414af3454b
vvoof <159160031+vvoof@users.noreply.github.com>
2025-03-01 14:08:18 -0500

    Created box inside canvas
    created box inside canvas

--- BoxBall.java
+++ BoxBall.java
@@ -1,4 +1,9 @@
 import java.awt.Color;
+import java.awt.Shape; 
+import java.awt.Rectangle;
+import java.awt.Graphics2D;
+import java.util.Random; 
+import java.util.ArrayList;
 
 /**
  * BoxBall models a ball that moves inside a box at random. The ball also 
@@ -10,23 +15,33 @@ import java.awt.Color;
 
 public class BoxBall
 {
-    // instance variables - replace the example below with your own
     private Canvas boxContainer; 
-
+    private ArrayList<Color> ballColor;
+    private Random speed; 
+    private Random position; 
+    private Random randomColor; 
+    private String boxColor; 
+    private Graphics2D graphic; 
+    
+    
     /**
      * Constructor for objects of class BoxBall
      */
     public BoxBall()
     {
-        // initialise instance variables
-        boxContainer = new Canvas("Box", 600, 500); 
-        int rightWall = 200;
-        int leftWall = 200; 
-        int bottomWall = 200;
+        boxContainer = new Canvas("Box", 600, 500, Color.white); 
         int topWall = 200; 
+        int bottomWall = 200;
+        int leftWall = 200;
+        int rightWall = 200; 
+        
+        boxContainer.setVisible(true); 
+        boxContainer.draw(new Rectangle
+                         (topWall, bottomWall, leftWall, rightWall));
+        
         
     }
-
+    
     /**
      * An example of a method - replace this comment with your own
      *
@@ -35,10 +50,15 @@ public class BoxBall
      */
     public void boxBounce ()
     {
-       int ground = 400; 
-       
-       boxContainer.setVisible(true); 
+    
        
        
+
     }
+    
+  
+    
+    
+    
+ 
 }
